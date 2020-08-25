abf07d2e9e54c1057b5455a0bdb38db1be9c45c3
vaberezin <vaberezin@protonmail.com>
2020-08-25 22:48:27 +0300

    Добавьте файлы проекта.

---
+++ Models/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л.Н. Толстой", Price = 220 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И.С. Тургенев", Price = 150 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А.П. Чехов", Price = 130 });
+
+            base.Seed(db);
+        }
+    }
+}
